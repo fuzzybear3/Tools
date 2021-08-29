9c33f8eda618ac705778aa89e7d8543fbda6b4f7
Steven Guido <stevenrguido@gmail.com>
2021-08-28 21:53:28 -0700

    fixed bashrc update script and added a few programs

--- startUp.sh
+++ startUp.sh
@@ -2,24 +2,43 @@
 
 
 #add script
-
-if $PATH | grep "Tools/scripts"
+if [ 2 -ge 3 ]
 then
-      echo "already added scripts to PATH"
+      echo "test"
 	  exit
 fi
 
-#echo $PATH:pwd
-echo "adding scripts to PATH"
-export PATH=$PATH:$PWD/scripts:
-echo $PATH
+$var echo $PATH | grep "Tools/scripts"
+if [ -n $var ]
+then
+    echo "already added scripts to PATH"
+else
+    echo "adding scripts to PATH"
+    #export PATH=$PATH:$PWD/scripts:
+    echo export PATH=$PATH:$PWD/scripts >> ~/.bashrc
+fi
 
 
+sudo apt update
+sudo apt upgrade
 
+#add-----------------
 
+#fzf
+echo yes Y | git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+echo yes Y | ~/.fzf/install
 
+#htop
+sudo apt-get install htop
 
-
+#tig
+sudo apt install tig
 
 #soft link
 #ln -s mklib.sh /bin
+
+echo "
+Added:
+tig: git tree viz
+htop: process manager
+fzf: (ctrl+r) history"
